814f9fc3646922c42e25b7f6fb05036d617c7c7a
rbolkhovitin <rbolkhovitin@gmail.com>
2019-11-27 13:25:17 +0300

    add few tests

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
+install:
+	poetry install --no-dev
+
+install-dev:
+	poetry install
 
 lint:
 	poetry run flake8 --exclude=lib
 
 mypy:
 	poetry run mypy --config-file mypy.ini -p minechat
+
+test:
+	poetry run pytest tests
---
+++ tests/helpers_test.py
@@ -0,0 +1,82 @@
+import asyncio
+
+import aionursery
+import pytest
+
+import minechat.helpers as helpers
+from minechat.exceptions import InvalidAddress
+
+
+@pytest.mark.asyncio
+async def test_reconnect_return():
+    count = 0
+
+    async def f():
+        nonlocal count
+        count += 1
+        return
+
+    f = helpers.reconnect(0)(f)
+    await f()
+    assert count == 1
+
+
+@pytest.mark.asyncio
+async def test_reconnect_connection_exceptions():
+    count = 0
+    want = 3
+
+    async def f():
+        nonlocal count
+        count += 1
+        if count < want:
+            raise ConnectionError
+        return
+
+    f = helpers.reconnect(0)(f)
+    await f()
+    assert count == want
+
+
+@pytest.mark.parametrize("address", ["host", "host:port"])
+def test_split_address_must_raise(address):
+    with pytest.raises(InvalidAddress):
+        helpers.split_address(address)
+
+
+@pytest.mark.parametrize(
+    "address,host,port",
+    [
+        ("10.0.2.170:8000", "10.0.2.170", 8000),
+        ("hostname:8080", "hostname", 8080),
+    ],
+    ids=["10.0.2.170:8000", "hostname:8080"]
+)
+def test_split_address_must_pass(address, host, port):
+    got_host, got_post = helpers.split_address(address)
+    assert got_host == host
+    assert got_post == port
+
+
+@pytest.mark.asyncio
+async def test_handy_nursery_single_exception():
+    async def f():
+        raise ValueError
+
+    with pytest.raises(ValueError):
+        async with helpers.create_handy_nursery() as nursery:
+            nursery.start_soon(f())
+
+
+@pytest.mark.asyncio
+async def test_handy_nursery_multiple_exception():
+    async def f():
+        raise ValueError
+
+    async def g():
+        raise KeyError
+
+    with pytest.raises(aionursery.MultiError):
+        async with helpers.create_handy_nursery() as nursery:
+            nursery.start_soon(f())
+            nursery.start_soon(g())
